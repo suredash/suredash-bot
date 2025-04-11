f5a4caecbd70afefc9fab2c454e751861d5dd542
suredash <suredash.bot@gmail.com>
2025-04-11 08:58:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
+const sendSignal = require('./sendSignal');
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
@@ -15,4 +16,20 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   if (msg.text.startsWith('/')) return;
   bot.sendMessage(chatId, `Recebido! Logo mais você verá suas surebets aqui. 😉`);
+});
+
+
+//Comando de TESTE
+bot.onText(/\/teste/, async (msg) => {
+  const data = {
+    casa1: 'Betano',
+    casa2: 'Pixbet',
+    odd1: 2.10,
+    odd2: 2.05,
+    jogo: 'Time A x Time B',
+    mercado: 'Resultado Final',
+  };
+
+  await sendSignal(bot, data);
+  bot.sendMessage(msg.chat.id, '✅ Sinal de teste enviado!');
 });
